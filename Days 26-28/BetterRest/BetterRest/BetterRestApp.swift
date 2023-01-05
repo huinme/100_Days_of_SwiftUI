fd8b3e27b0cadf181f7746b5a42a79a425a189ff
huin <koichi.skt@gmail.com>
2023-01-05 16:47:59 +0900

    create BetterRest project

---
+++ Days 26-28/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by huin on 2023/01/05.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

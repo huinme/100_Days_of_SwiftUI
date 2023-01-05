8aaf7b0de55e1f8e66c311f6151144935cdb118d
huin <koichi.skt@gmail.com>
2023-01-05 11:07:02 +0900

    create project

---
+++ Days 20-22/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by huin on 2023/01/05.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

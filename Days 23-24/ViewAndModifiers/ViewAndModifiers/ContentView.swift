24933c4ef9d185fa0f088573c8d569725ebcc914
huin <koichi.skt@gmail.com>
2023-01-05 16:25:36 +0900

    finished up day 23

---
+++ Days 23-24/ViewAndModifiers/ViewAndModifiers/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  ViewAndModifiers
+//
+//  Created by huin on 2023/01/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        GridStack(rows: 4, columns: 4) { row, col in
+            Image(systemName: "\(row * 4 + col).circle")
+            Text("R\(row) C\(col)")
+        }
+    }
+}
+
+struct GridStack<Content: View>: View {
+
+    let rows: Int
+    let columns: Int
+    @ViewBuilder let content: (Int, Int) -> Content
+
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice(PreviewDevice(rawValue: "iPhone 14 Pro"))
+    }
+}
---
+++ Days 23-24/ViewAndModifiers/ViewAndModifiers/ViewAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewAndModifiersApp.swift
+//  ViewAndModifiers
+//
+//  Created by huin on 2023/01/05.
+//
+
+import SwiftUI
+
+@main
+struct ViewAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

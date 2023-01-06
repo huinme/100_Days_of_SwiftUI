8b8cf1a02313318f148ecfa93686f0931042f5a6
huin <koichi.skt@gmail.com>
2023-01-06 12:17:41 +0900

    finish: Customizing animations in SwiftUI

--- Day 32-34/Animations/Animations/ContentView.swift
+++ Day 32-34/Animations/Animations/ContentView.swift
@@ -8,19 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var animationAmount = 1.0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button("Tap me") {
+            animationAmount += 1
         }
-        .padding()
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
+        )
+        .onAppear {
+            animationAmount += 2
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice(PreviewDevice(rawValue: "iPhone 14 Pro"))
     }
 }

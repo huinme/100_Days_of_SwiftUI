a80582e01555da6671a75b94f638bbab2090706c
huin <koichi.skt@gmail.com>
2023-01-06 11:15:58 +0900

    finished up day 30

---
+++ Days 29-31/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by huin on 2023/01/06.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

f7cbc101c531b17d8cbb64b51fabc3cfa2d715d3
huin <koichi.skt@gmail.com>
2023-01-07 16:29:12 +0900

    Finishing up with one last view

---
+++ Day 39-42/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by huin on 2023/01/07.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Day 39-42/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by huin on 2023/01/07.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

4153d619a36ac70862cc7b0ff09f94e21f3cbf5d
huin <koichi.skt@gmail.com>
2023-01-08 14:16:41 +0900

    create project 12

---
+++ Day 57-59/CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by huin on 2023/01/08.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

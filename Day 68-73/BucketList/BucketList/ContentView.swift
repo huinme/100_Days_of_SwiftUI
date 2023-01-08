9fed719a6cf77d386bdaac26c24e42b7eefc882e
huin <koichi.skt@gmail.com>
2023-01-08 21:37:00 +0900

    finish day 73

--- Day 68-73/BucketList/BucketList/ContentView.swift
+++ Day 68-73/BucketList/BucketList/ContentView.swift
@@ -7,71 +7,141 @@
 
 import SwiftUI
 import MapKit
+import LocalAuthentication
 
 struct ContentView: View {
 
-    @State private var mapRegion = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 50, longitude: 0),
-        span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25)
-    )
-    @State private var locations = [Location]()
-    @State private var selectedPlace: Location?
+    @MainActor class ViewModel: ObservableObject {
+
+        let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedPlaces")
+
+        @Published var isUnlocked = false
+        @Published var mapRegion = MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 50, longitude: 0),
+            span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25)
+        )
+        @Published private(set) var locations: [Location]
+        @Published var selectedPlace: Location?
+
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath)
+                locations = try JSONDecoder().decode([Location].self, from: data)
+            } catch {
+                locations = []
+            }
+        }
+
+        func authenticate() {
+            let context = LAContext()
+            var error: NSError?
+
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                let reason = "Please authenticate yourself to unlock your places."
+
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+
+                    if success {
+                        Task { @MainActor in
+                            self.isUnlocked = true
+                        }
+                    } else {
+                        // error
+                    }
+                }
+            } else {
+                // no biometrics
+            }
+        }
+
+        func addLocation() {
+            // create a new location
+            let newLocation = Location(id: UUID(),
+                                       name: "New location",
+                                       description: "",
+                                       latitude: mapRegion.center.latitude,
+                                       longitude: mapRegion.center.longitude)
+            locations.append(newLocation)
+            save()
+        }
+
+        func update(location: Location) {
+            guard let selectedPlace = selectedPlace else { return }
+
+            if let index = locations.firstIndex(of: selectedPlace) {
+                locations[index] = location
+            }
+        }
+
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomic, .completeFileProtection])
+            } catch {
+                print("Unable to save data.")
+            }
+        }
+    }
+
+    @StateObject private var viewModel = ViewModel()
 
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-                MapAnnotation(coordinate: location.coordinate) {
-                    VStack {
-                        Image(systemName: "star.circle")
-                            .resizable()
-                            .foregroundColor(.red)
-                            .frame(width: 44, height: 44)
-                            .background(.white)
-                            .clipShape(Circle())
-                        Text(location.name)
-                            .fixedSize()
-                    }
-                    .onTapGesture {
-                        selectedPlace = location
+            if viewModel.isUnlocked {
+                Map(coordinateRegion: $viewModel.mapRegion, annotationItems: viewModel.locations) { location in
+                    MapAnnotation(coordinate: location.coordinate) {
+                        VStack {
+                            Image(systemName: "star.circle")
+                                .resizable()
+                                .foregroundColor(.red)
+                                .frame(width: 44, height: 44)
+                                .background(.white)
+                                .clipShape(Circle())
+                            Text(location.name)
+                                .fixedSize()
+                        }
+                        .onTapGesture {
+                            viewModel.selectedPlace = location
+                        }
                     }
                 }
-            }
-            .ignoresSafeArea()
+                .ignoresSafeArea()
 
-            Circle()
-                .fill(.blue)
-                .opacity(0.3)
-                .frame(width: 32, height: 32)
+                Circle()
+                    .fill(.blue)
+                    .opacity(0.3)
+                    .frame(width: 32, height: 32)
 
-            VStack {
-                Spacer()
-                HStack {
+                VStack {
                     Spacer()
-                    Button {
-                        // create a new location
-                        let newLocation = Location(id: UUID(),
-                                                   name: "New location",
-                                                   description: "",
-                                                   latitude: mapRegion.center.latitude,
-                                                   longitude: mapRegion.center.longitude)
-                        locations.append(newLocation)
-                    } label: {
-                        Image(systemName: "plus")
+                    HStack {
+                        Spacer()
+                        Button {
+                            viewModel.addLocation()
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                        .padding()
+                        .background(.black.opacity(0.75))
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .clipShape(Circle())
+                        .padding(.trailing)
                     }
-                    .padding()
-                    .background(.black.opacity(0.75))
-                    .foregroundColor(.white)
-                    .font(.title)
-                    .clipShape(Circle())
-                    .padding(.trailing)
                 }
+            } else {
+                Button("Unlock Places") {
+                    viewModel.authenticate()
+                }
+                .padding()
+                .background(.blue)
+                .foregroundColor(.white)
+                .clipShape(Capsule())
             }
         }
-        .sheet(item: $selectedPlace) { place in
-            EditView(location: place) { newLocation in
-                if let index = locations.firstIndex(of: place) {
-                    locations[index] = newLocation
-                }
+        .sheet(item: $viewModel.selectedPlace) { place in
+            EditView(location: place) {
+                viewModel.update(location: $0)
             }
         }
     }

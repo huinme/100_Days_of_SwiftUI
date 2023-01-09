b61e62b117c3ea98b715d4b8226041abf39c2a77
huin <koichi.skt@gmail.com>
2023-01-09 12:01:39 +0900

    Timer

--- Day 86-91/Flashzilla/Flashzilla/ContentView.swift
+++ Day 86-91/Flashzilla/Flashzilla/ContentView.swift
@@ -9,17 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
 
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var counter = 0
+
     var body: some View {
         VStack {
-            Text("Hello")
-            Spacer().frame(height: 100)
-            Text("World")
+            Text("Hello, World")
+                .onReceive(timer) { time in
+                    if counter == 5 {
+                        timer.upstream.connect().cancel()
+                    } else {
+                        print("The time is now \(time)")
+                    }
+                    counter += 1
+                }
         }
-        .contentShape(Rectangle())
-        .onTapGesture {
-            print("VStack tapped!")
-        }
-
     }
 }
 

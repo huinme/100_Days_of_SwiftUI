de54fd05298047491dc1e9333c422cee6887dd42
huin <koichi.skt@gmail.com>
2023-01-09 12:04:33 +0900

    ScenePhase

--- Day 86-91/Flashzilla/Flashzilla/ContentView.swift
+++ Day 86-91/Flashzilla/Flashzilla/ContentView.swift
@@ -9,21 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
 
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    @State private var counter = 0
+    @Environment(\.scenePhase) var scenePhase
 
     var body: some View {
-        VStack {
-            Text("Hello, World")
-                .onReceive(timer) { time in
-                    if counter == 5 {
-                        timer.upstream.connect().cancel()
-                    } else {
-                        print("The time is now \(time)")
-                    }
-                    counter += 1
+        Text("Hello, world!")
+            .padding()
+            .onChange(of: scenePhase) { newPhase in
+                if newPhase == .active {
+                    print("Active")
+                } else if newPhase == .inactive {
+                    print("Inactive")
+                } else if newPhase == .background {
+                    print("Background")
                 }
-        }
+            }
     }
 }
 

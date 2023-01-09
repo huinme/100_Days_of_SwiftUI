257c5ed3b6e10c13415294b0ecdb8c39756c730a
huin <koichi.skt@gmail.com>
2023-01-09 09:05:21 +0900

    create AccessibilitySandbox project

---
+++ Day 74-76/AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by huin on 2023/01/09.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

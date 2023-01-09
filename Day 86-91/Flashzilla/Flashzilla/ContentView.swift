97efca2b513e4bdad4cd58e7377a00528ad19a2e
huin <koichi.skt@gmail.com>
2023-01-09 11:48:58 +0900

    use CoreHaptics

--- Day 86-91/Flashzilla/Flashzilla/ContentView.swift
+++ Day 86-91/Flashzilla/Flashzilla/ContentView.swift
@@ -6,82 +6,66 @@
 //
 
 import SwiftUI
-
-struct MagnificationGestureView: View {
-
-    @State private var currentAmount = 0.0
-    @State private var finalAmount = 1.0
-
-    var body: some View {
-        VStack {
-            Text("Hello, world!")
-                .scaleEffect(finalAmount + currentAmount)
-                .gesture(
-                    MagnificationGesture()
-                        .onChanged { amount in
-                            currentAmount = amount - 1
-                        }
-                        .onEnded { amount in
-                            finalAmount += currentAmount
-                            currentAmount = 0
-                        }
-                )
-        }
-        .padding()
-    }
-}
-
-struct RotationGestureView: View {
-
-    @State private var currentAmount = Angle.zero
-    @State private var finalAmount = Angle.zero
-
-    var body: some View {
-        Text("Hello, World!")
-            .rotationEffect(currentAmount + finalAmount)
-            .gesture(
-                RotationGesture()
-                    .onChanged { angle in
-                        currentAmount = angle
-                    }
-                    .onEnded { angle in
-                        finalAmount += currentAmount
-                        currentAmount = .zero
-                    }
-            )
-    }
-}
+import CoreHaptics
 
 struct ContentView: View {
 
-    @State private var offset = CGSize.zero
-    @State private var isDragging = false
+    @State private var engine: CHHapticEngine?
 
     var body: some View {
-        let dragGesture = DragGesture()
-            .onChanged { value in offset = value.translation }
-            .onEnded { _ in
-                withAnimation {
-                    offset = .zero
-                    isDragging = false
-                }
-            }
+        VStack(spacing: 16) {
+            Text("Hello, World!")
+                .onAppear(perform: prepareHaptics)
+                .onTapGesture(perform: complexSuccess)
+        }
+    }
 
-        let pressGesture = LongPressGesture()
-            .onEnded { value in
-                withAnimation {
-                    isDragging = true
-                }
-            }
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
 
-        let combined = pressGesture.sequenced(before: dragGesture)
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
 
-        Circle()
-            .fill(.red)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combined)
+    func complexSuccess() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else {
+            print("hoge")
+            return
+        }
+        var events = [CHHapticEvent]()
+
+        // create one intense, sharp tap
+//        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+//        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+//        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+//        events.append(event)
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
+
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append(event)
+        }
+
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
     }
 }
 

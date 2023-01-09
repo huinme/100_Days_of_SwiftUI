546f5fac3f688c000804b657dd118ce409a6ffb6
huin <koichi.skt@gmail.com>
2023-01-09 13:14:43 +0900

    day 89

--- Day 86-91/Flashzilla/Flashzilla/ContentView.swift
+++ Day 86-91/Flashzilla/Flashzilla/ContentView.swift
@@ -18,8 +18,12 @@ extension View {
 struct ContentView: View {
 
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.scenePhase) var scenePhase
 
     @State private var cards = [Card](repeating: Card.example, count: 10)
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var isActive = true
 
     var body: some View {
         ZStack {
@@ -28,6 +32,14 @@ struct ContentView: View {
                 .ignoresSafeArea()
 
             VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(Capsule())
+
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: cards[index]) {
@@ -37,6 +49,15 @@ struct ContentView: View {
                         }
                         .stacked(at: index, in: cards.count)
                     }
+                }
+                .allowsHitTesting(timeRemaining > 0)
+
+                if cards.isEmpty {
+                    Button("Start Again", action: resetCards)
+                        .padding()
+                        .background(.white)
+                        .foregroundColor(.black)
+                        .clipShape(Capsule())
                 }
             }
 
@@ -50,7 +71,7 @@ struct ContentView: View {
                             .background(.black.opacity(0.7))
                             .clipShape(Circle())
                         Spacer()
-                        Image(systemName: "chrckmark.circle")
+                        Image(systemName: "checkmark.circle")
                             .padding()
                             .background(.black.opacity(0.7))
                             .clipShape(Circle())
@@ -61,10 +82,36 @@ struct ContentView: View {
                 }
             }
         }
+        .onReceive(timer) { time in
+            guard isActive else { return }
+
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
+        }
+        .onChange(of: scenePhase) { newPhase in
+            if newPhase == .active {
+                if cards.isEmpty == false {
+                    isActive = true
+                }
+            } else {
+                isActive = false
+            }
+        }
     }
 
     func removeCard(at index: Int) {
         cards.remove(at: index)
+
+        if cards.isEmpty {
+            isActive = false
+        }
+    }
+
+    func resetCards() {
+        cards = [Card](repeating: Card.example, count: 10)
+        timeRemaining = 100
+        isActive = true
     }
 }
 

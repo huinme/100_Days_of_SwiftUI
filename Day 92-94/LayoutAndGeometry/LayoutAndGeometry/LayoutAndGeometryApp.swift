92f7f0132ac690c7c9c1764f92290d3ba8e3c5b2
huin <koichi.skt@gmail.com>
2023-01-10 21:23:36 +0900

    Day 93

---
+++ Day 92-94/LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by huin on 2023/01/10.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

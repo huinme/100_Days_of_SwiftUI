f6ab3ddb9829d5c1a89fb06a4343a6e7e596dfd1
huin <koichi.skt@gmail.com>
2023-01-14 13:23:03 +0900

    Day 98

--- Day 96-99/SnowSneaker/SnowSneaker/ContentView.swift
+++ Day 96-99/SnowSneaker/SnowSneaker/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
 
     @State private var resorts: [Resort] = Bundle.main.decode("resorts.json")
     @State private var searchText = ""
+    @StateObject var favorites = Favorites()
 
     var body: some View {
         NavigationView {
@@ -18,21 +19,30 @@ struct ContentView: View {
                 NavigationLink {
                     ResortView(resort: resort)
                 } label: {
-                    Image(resort.country)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 40, height: 25)
-                        .clipShape(RoundedRectangle(cornerRadius: 5))
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 5)
-                                .stroke(.black, lineWidth: 1)
-                        )
-
-                    VStack(alignment: .leading) {
-                        Text(resort.name)
-                            .font(.headline)
-                        Text("\(resort.runs) runs")
-                            .foregroundColor(.secondary)
+                    HStack {
+                        Image(resort.country)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 40, height: 25)
+                            .clipShape(RoundedRectangle(cornerRadius: 5))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(.black, lineWidth: 1)
+                            )
+                        
+                        VStack(alignment: .leading) {
+                            Text(resort.name)
+                                .font(.headline)
+                            Text("\(resort.runs) runs")
+                                .foregroundColor(.secondary)
+                        }
+                        
+                        if favorites.contains(resort) {
+                            Spacer()
+                            Image(systemName: "heart.fill")
+                                .accessibilityLabel("This is a favorite resort")
+                                .foregroundColor(.red)
+                        }
                     }
                 }
             }
@@ -41,6 +51,7 @@ struct ContentView: View {
 
             WelcomeView()
         }
+        .environmentObject(favorites)
     }
 
     var filteredResorts: [Resort] {
